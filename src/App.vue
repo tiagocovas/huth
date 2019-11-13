724a3ca09da0f3343ec10ea2b05c62e8daf4b988
Tiago Covas <tiago.covas@gmail.com>
2019-11-13 15:18:40 +0000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+/*eslint no-console: "error"*/
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+
+body {
+  background: linear-gradient(#e8f5ec 0%,
+                              #d9f0e6 10%, 
+                              #d4e9d6 20%, 
+                              #c8ebe7 30%,
+                              #b8eaf5 40%,
+                              #c8def6 50%,
+                              #cad8f3 60%,
+                              #bbaedc 70%,
+                              #c0addb 80%,
+                              #3681a8 90%);
+  height: 1000vh;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+  
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="background">
+    <h1>{{ msg }}</h1>
+    <img class="head-1" :style="head1Style" alt="head1" src="./../assets/head1.png">
+  </div>
+</template>
+
+<script>
+export default {
+  /*eslint no-console: "error"*/
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+
+  data () {
+    return {
+      head1Style: ''
+    };
+  },
+
+  methods: {
+    updateScroll() {
+      this.head1Style = `top: ${0.534*window.scrollY}px; left: ${0.822*window.scrollY}px;`
+    }
+  },
+
+  mounted() {
+    window.addEventListener('scroll', this.updateScroll);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped >
+
+.head-1 {
+  width: 10vw;
+  
+  position: absolute;
+  left: 0;
+  top: 0;
+  transition: all 0.2s ease-in;
+}
+</style>
